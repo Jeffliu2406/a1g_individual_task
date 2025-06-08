407dea734b02e92b2f026161c86d9727fa484f04
Jeffliu2406 <jliu0849@uni.sydney.edu.au>
2025-06-08 21:43:42 +1000

    second version

--- sketch.js
+++ sketch.js
@@ -1,48 +1,106 @@
+
 let stripes = [];
 let currentStripe = 0;
+let rangeX;
+let rangeY;
+let rangeLength;
+let mode = 1;
+let buttonW = 200;
+let buttonH = 48;
 
 function setup() {
-  createCanvas(1440, 800);
-  background(240, 240, 225);
+  createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
+  background(240, 240, 225);
+  rangeX = windowWidth * 0.3;
+  rangeY = windowHeight * 0.3;
+  rangeLength = windowWidth * 0.5;
 
-  let baseAngles = [0, 90, -90]; //Angle settings
-
+  let baseAngles = mode === 0 ? [0, 90, -90] : [0];
   for (let i = 0; i < 100; i++) {
     stripes.push(new LineStripe(
-      random(-800, 500),        // x
-      random(-500, 500),        // y
-      random(100, 2000),        // length
-      random(0.1, 5),           // spacing
-      floor(random(6, 50)),     // number of lines
-      random(baseAngles),       // angle
-      random(0.3, 3)            // base weight
+      random(-rangeX, rangeX),
+      random(-rangeY, rangeY),
+      random(20, rangeLength),
+      random(0.1, 8),
+      floor(random(6, 50)),
+      random(baseAngles),
+      random(0.1, 1)
     ));
   }
-
-  
 }
 
 function draw() {
-  translate(width / 2, height / 2);  
-  
-  //Generate line groups through loop
+  translate(width / 2, height / 2);
   if (currentStripe < stripes.length) {
     stripes[currentStripe].displayStep();
-    if (stripes[currentStripe].done) {
-      currentStripe++;
-    }
-  } else {
-    noLoop(); //Stop looping
+    if (stripes[currentStripe].done) currentStripe++;
+  }
+
+  for (let stripe of stripes) {
+    stripe.update();
+    stripe.display();
+  }
+
+  drawModeButton();
+}
+
+function drawModeButton() {
+  push();
+  resetMatrix();
+  let x = 20;
+  let y = height - buttonH - 20;
+  fill(255, 230, 180, 220);
+  stroke(120);
+  strokeWeight(2);
+  rect(x, y, buttonW, buttonH, 12);
+  fill(60);
+  noStroke();
+  textSize(20);
+  textAlign(CENTER, CENTER);
+  text(mode === 1 ? "Switch to cross" : "Switch to parallel", x + buttonW / 2, y + buttonH / 2);
+  pop();
+}
+
+function mousePressed() {
+  let x = 20;
+  let y = height - buttonH - 20;
+  if (mouseX >= x && mouseX <= x + buttonW && mouseY >= y && mouseY <= y + buttonH) {
+    mode = mode === 1 ? 0 : 1;
+    stripes = [];
+    currentStripe = 0;
+    loop();
+    setup();
   }
 }
 
+function keyPressed() {
+  if (key === ' ') {
+    for (let s of stripes) s.explode();
+  }
+}
 
-//Line group class
+function mouseMoved() {
+  for (let s of stripes) {
+    let d = dist(mouseX - width / 2, mouseY - height / 2, s.x, s.y);
+    if (d < 80) {
+      let angle = atan2(s.y - (mouseY - height / 2), s.x - (mouseX - width / 2));
+      s.vx += cos(angle) * 2;
+      s.vy += sin(angle) * 2;
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 class LineStripe {
   constructor(x, y, len, spacing, count, angle, baseWeight) {
     this.x = x;
     this.y = y;
+    this.originX = x;
+    this.originY = y;
     this.len = len;
     this.spacing = spacing;
     this.count = count;
@@ -51,32 +109,69 @@ class LineStripe {
     this.lines = [];
     this.index = 0;
     this.done = false;
+    this.currentLen = 0;
+    this.gray = random(10, 200);
+    this.vx = 0;
+    this.vy = 0;
 
     for (let i = 0; i < this.count; i++) {
       let offsetY = i * this.spacing;
       let opacity = random(2, 100);
       let weight = this.baseWeight + random(-0.1, 0.5);
-      this.lines.push({ offsetY, opacity, weight });
+      let m = round(random(3));
+      this.lines.push({ offsetY, opacity, weight, m });
     }
   }
-  
-  //Draw line group function
+
   displayStep() {
     push();
     translate(this.x, this.y);
     rotate(-this.angle);
-
-    if (this.index < this.lines.length) {
-      let l = this.lines[this.index];
-      let gray = random(2, 180); // Grayscale color
-      stroke(gray, l.opacity); // Color opacity change
-      strokeWeight(l.weight); // Line thickness change
-      line(0+l.offsetY, l.offsetY, this.len+l.offsetY, l.offsetY); //生成线段
-      this.index++; // Increase number of lines
+    for (let i = 0; i < this.lines.length; i++) {
+      let l = this.lines[i];
+      stroke(this.gray, l.opacity);
+      strokeWeight(l.weight);
+      if (l.m === 0) {
+        line(0 + l.offsetY, l.offsetY, this.currentLen + l.offsetY, l.offsetY);
+      } else {
+        line(this.len + l.offsetY, l.offsetY, this.len - this.currentLen + l.offsetY, l.offsetY);
+      }
+    }
+    if (this.currentLen < this.len) {
+      this.currentLen += 10;
     } else {
       this.done = true;
     }
-
     pop();
+  }
+
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+
+    if (this.x < -width / 2 || this.x > width / 2) this.vx *= -0.85;
+    if (this.y < -height / 2 || this.y > height / 2) this.vy *= -0.85;
+
+    this.vx *= 0.98;
+    this.vy *= 0.98;
+  }
+
+  display() {
+    push();
+    translate(this.x, this.y);
+    rotate(-this.angle);
+    for (let i = 0; i < this.lines.length; i++) {
+      let l = this.lines[i];
+      stroke(this.gray, l.opacity);
+      strokeWeight(l.weight);
+      line(0 + l.offsetY, l.offsetY, this.len + l.offsetY, l.offsetY);
+    }
+    pop();
+  }
+
+  explode() {
+    let angle = random(TWO_PI);
+    this.vx = cos(angle) * random(8, 14);
+    this.vy = sin(angle) * random(2, 6);
   }
 }
